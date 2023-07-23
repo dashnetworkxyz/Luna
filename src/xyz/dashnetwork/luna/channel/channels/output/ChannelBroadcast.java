fa8fdb1a9ffff70ce1ca311de80cbf8ed295f5ce
MasterDash5 <andrewbell25@icloud.com>
2023-07-22 22:01:42 -0600

    License under GNU GPLv3

--- src/xyz/dashnetwork/luna/channel/channels/output/ChannelBroadcast.java
+++ src/xyz/dashnetwork/luna/channel/channels/output/ChannelBroadcast.java
@@ -3,8 +3,9 @@
  * Copyright (C) 2023  DashNetwork
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
